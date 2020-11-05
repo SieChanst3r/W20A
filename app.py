19fef0a3967f6e5ee233577b1047627ac2d0a1b1
SieChan <sie.chan@outlook.com>
2020-11-04 17:55:40 -0700

    create post function added

--- app.py
+++ app.py
@@ -7,4 +7,11 @@ import mariadb
 def createPost():
     content = input("What is on your mind? insert here: ")
     conn =mariadb.connect(user = dbcreds.user, password = dbcreds.password, host = dbcreds.host, port = dbcreds.port, database = dbcerds.database)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO blog_post(username, content, id) VALUES(?, ?, NULL)", [username, content])
+    conn.commit()
+    print("Your Post has beed Created!")
+    cursor.close()
+    conn.close()
+
     
